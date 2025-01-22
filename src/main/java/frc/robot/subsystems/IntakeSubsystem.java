609022b4f060ed994265aba952f94b5fa27442a9
amzoeee <zoe.z.zhao@gmail.com>
2025-01-21 18:29:32 -0800

    remove unused imports, print statement, outdated comments

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -3,8 +3,6 @@ package frc.robot.subsystems;
 import au.grapplerobotics.LaserCan;
 import au.grapplerobotics.CanBridge;
 
-import au.grapplerobotics.ConfigurationFailedException;
-import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -19,8 +17,6 @@ public class IntakeSubsystem extends SubsystemBase {
     lc = new LaserCan(IntakeConstants.LASER_CAN_ID);
   }
 
-  //to-do: smartdashboard print these values instead of system.out.print.ln
-  //use grapple hook; test it out; connect; set the CAN id
   @Override
   public void periodic() {
     LaserCan.Measurement measurement = lc.getMeasurement();
@@ -28,7 +24,6 @@ public class IntakeSubsystem extends SubsystemBase {
       SmartDashboard.putNumber("LaserCAN Distance", measurement.distance_mm);
     } else {
       SmartDashboard.putNumber("LaserCAN Distance", 0);
-      // System.out.println("Oh no! The target is out of range, or we can't get a reliable measurement!");
       // You can still use distance_mm in here, if you're ok tolerating a clamped value or an unreliable measurement.
     }
   }
