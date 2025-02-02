6b1c19fb6a2b4fc592b05aad9571771963ad5644
riley <rileykjen@gmail.com>
2025-02-02 13:59:52 -0800

    rename to LifterSubsystem

--- src/main/java/frc/robot/subsystems/lifter/ElevatorAndArmSubsystem.java
+++
@@ -1,6 +0,0 @@
-package frc.robot.subsystems.lifter;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
-public class ElevatorAndArmSubsystem extends SubsystemBase {
-    
-}
---
+++ src/main/java/frc/robot/subsystems/lifter/LifterSubsystem.java
@@ -0,0 +1,10 @@
+package frc.robot.subsystems.lifter;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class LifterSubsystem extends SubsystemBase {
+    
+    // constructor
+    public LifterSubsystem() {
+
+    }
+}
