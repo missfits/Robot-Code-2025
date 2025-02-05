16194d0040aeb6aea66a18cb9792b90225fe389f
riley <rileykjen@gmail.com>
2025-02-04 21:14:25 -0800

    add constants
    also separated lifter constants into elevator and arm constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -44,13 +44,13 @@ public final class Constants {
     public static final double WHEEL_RADIUS_FUDGE_FACTOR = 0.96153846153; // approximated @ PF session 1/13
   }
 
-  public static class LifterConstants {
+  public static class ElevatorConstants {
     public static final int ELEVATOR_MOTOR_ID = 21; // placeholder
-    public static final int ARM_MOTOR_ID = 22; // placeholder
-
+  
     public static final int COUNTS_PER_REV = 42; // may need to be updated
 
     public static final double METERS_PER_ROTATION = 0.0; 
+    
     public static final double MAX_SPEED = 0.0; 
     public static final double SPEED_LOWER_LIMIT = 0.0;
     public static final double SPEED_UPPER_LIMIT = 0.0;
@@ -61,14 +61,27 @@ public final class Constants {
     public static final double kV = 12/MAX_SPEED; // may need to be updated
     public static final double kA = 0;
 
-    public static final double ELEVATOR_P = 0; 
-    public static final double ELEVATOR_I = 0;
-    public static final double ELEVATOR_D = 0; 
+    public static final double kP = 0; 
+    public static final double kI = 0;
+    public static final double kD = 0; 
 
+    public static final double kMaxV = 0;
+    public static final double kMaxA = 0; 
 
-    public static final double ARM_P = 0;
-    public static final double ARM_I = 0;
-    public static final double ARM_D = 0;
+  }
+
+  public static class ArmConstants {
+    public static final int ARM_MOTOR_ID = 22; // placeholder
+    public static final int COUNTS_PER_REV = 42; // may need to be updated
+
+    public static final double METERS_PER_ROTATION = 0.0; 
+    public static final double MAX_SPEED = 0.0; 
+    public static final double SPEED_LOWER_LIMIT = 0.0;
+    public static final double SPEED_UPPER_LIMIT = 0.0;
+
+    public static final double kP = 0;
+    public static final double kI = 0;
+    public static final double kD = 0;
   }
 
   public static class CollarConstants {
