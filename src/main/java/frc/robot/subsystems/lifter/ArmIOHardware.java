6bb1396fb0388e6c50a4bb8417d37928e990f0b4
riley <rileykjen@gmail.com>
2025-02-04 21:13:27 -0800

    add elevator/arm IO motor functions

--- src/main/java/frc/robot/subsystems/lifter/ArmIOHardware.java
+++ src/main/java/frc/robot/subsystems/lifter/ArmIOHardware.java
@@ -1,22 +1,49 @@
 package frc.robot.subsystems.lifter;
 
-import com.ctre.phoenix6.hardware.TalonFX;
-
-import frc.robot.Constants.LifterConstants;
-
-import edu.wpi.first.units.measure.*;
 import static edu.wpi.first.units.Units.*;
 
-public class ArmIOHardware implements ArmIO {
-    private final TalonFX m_armMotor = new TalonFX(LifterConstants.ARM_MOTOR_ID);
+import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.controls.VoltageOut;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import frc.robot.Constants.ArmConstants;
+
+public class ArmIOHardware {
+    private final TalonFX m_armMotor = new TalonFX(ArmConstants.ARM_MOTOR_ID);
 
     // constructor
-    public ArmIOHardware() {
+    public ArmIOHardware() {}
 
+    // getters
+    public double getPosition() {
+        double angle = m_armMotor.getPosition().getValue().in(Revolutions);
+        return angle*ArmConstants.METERS_PER_ROTATION;
     }
 
-    public double getPositionMeters() {
-        double angle = m_armMotor.getPosition().getValue().in(Revolutions);
-        return angle*LifterConstants.METERS_PER_ROTATION;
+    public double getVelocity() {
+        double speed = m_armMotor.getVelocity().getValue().in(RevolutionsPerSecond);
+        return speed*ArmConstants.METERS_PER_ROTATION;
+    }
+
+    // setters
+    public void motorOff() {
+        m_armMotor.setVoltage(0);
+        m_armMotor.stopMotor();
+    }
+
+    public void setPosition(double value) {
+        m_armMotor.setPosition(value);
+    }
+
+    public void resetPosition() {
+        setPosition(0);
+    }
+
+    public void setVoltage(double value) {
+        m_armMotor.setControl(new VoltageOut(value));
+    }
+    
+    public void setVoltage(PositionVoltage request) {
+        m_armMotor.setControl(request);
     }
 }
--- src/main/java/frc/robot/subsystems/lifter/ElevatorIOHardware.java
+++ src/main/java/frc/robot/subsystems/lifter/ElevatorIOHardware.java
@@ -1,22 +1,49 @@
 package frc.robot.subsystems.lifter;
 
-import com.ctre.phoenix6.hardware.TalonFX;
-
-import frc.robot.Constants.LifterConstants;
-
-import edu.wpi.first.units.measure.*;
 import static edu.wpi.first.units.Units.*;
 
-public class ElevatorIOHardware implements ElevatorIO {
-    private final TalonFX m_elevatorMotor = new TalonFX(LifterConstants.ELEVATOR_MOTOR_ID);
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.controls.*;
+
+import frc.robot.Constants.ElevatorConstants;
+
+public class ElevatorIOHardware {
+    private final TalonFX m_elevatorMotor = new TalonFX(ElevatorConstants.ELEVATOR_MOTOR_ID);
     
     // constructor
-    public ElevatorIOHardware() {
+    public ElevatorIOHardware() {}
 
+    // ----- MOTOR METHODS -----
+    // getters
+    public double getPosition() {
+        double angle = m_elevatorMotor.getPosition().getValue().in(Revolutions);
+        return angle*ElevatorConstants.METERS_PER_ROTATION;
     }
 
-    public double getPositionMeters() {
-        double angle = m_elevatorMotor.getPosition().getValue().in(Revolutions);
-        return angle*LifterConstants.METERS_PER_ROTATION;
+    public double getVelocity() {
+        double speed = m_elevatorMotor.getVelocity().getValue().in(RevolutionsPerSecond);
+        return speed*ElevatorConstants.METERS_PER_ROTATION;
+    }
+
+    // setters
+    public void motorOff() {
+        m_elevatorMotor.setVoltage(0);
+        m_elevatorMotor.stopMotor();
+    }
+
+    public void setPosition(double value) {
+        m_elevatorMotor.setPosition(value);
+    }
+
+    public void resetPosition() {
+        setPosition(0);
+    }
+
+    public void setVoltage(double value) {
+        m_elevatorMotor.setControl(new VoltageOut(value));
+    }
+    
+    public void setVoltage(PositionVoltage request) {
+        m_elevatorMotor.setControl(request);
     }
 }
