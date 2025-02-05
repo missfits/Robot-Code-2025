6f41dff700590727816226b5c1cc380e3cb6cc80
riley <rileykjen@gmail.com>
2025-02-04 21:14:04 -0800

    create arm subsystem

---
+++ src/main/java/frc/robot/subsystems/lifter/ArmSubsystem.java
@@ -0,0 +1,9 @@
+package frc.robot.subsystems.lifter;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ArmSubsystem extends SubsystemBase {
+    private final ArmIOHardware m_armIO = new ArmIOHardware();
+
+    // constructor
+    public ArmSubsystem() {}
+}
