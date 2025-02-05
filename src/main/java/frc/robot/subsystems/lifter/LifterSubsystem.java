70ee835781a5db0c443ca706321f3fe606a35bfb
riley <rileykjen@gmail.com>
2025-02-04 21:15:00 -0800

    update lifter subsystem skeleton

--- src/main/java/frc/robot/subsystems/lifter/LifterSubsystem.java
+++ src/main/java/frc/robot/subsystems/lifter/LifterSubsystem.java
@@ -1,25 +1,27 @@
 package frc.robot.subsystems.lifter;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-import frc.robot.RobotContainer.RobotState;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 
-import com.ctre.phoenix6.hardware.TalonFX;
-import frc.robot.Constants.LifterConstants;
+import frc.robot.RobotContainer.RobotState;
+import frc.robot.Constants.ElevatorConstants;
+import frc.robot.Constants.ArmConstants;
 
 
 public class LifterSubsystem extends SubsystemBase {
-    private final ElevatorIOHardware m_elevatorIO = new ElevatorIOHardware();
-    private final ArmIOHardware m_armIO = new ArmIOHardware();
-
+    private final ElevatorSubsystem m_elevator = new ElevatorSubsystem();
+    private final ArmSubsystem m_arm = new ArmSubsystem();
 
     // constructor
-    public LifterSubsystem() {
-
-    }
+    public LifterSubsystem() {}
 
     public Command getCommand(RobotState targetRobotState) {
         return new WaitCommand(0);
     }
+
+    public Command moveToCommand(RobotState targetRobotState) {
+        return new WaitCommand(0);
+    }
+
 }
