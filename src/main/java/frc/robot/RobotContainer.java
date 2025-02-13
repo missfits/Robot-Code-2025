acaec5bab963a5c818cc270ad87a0fbbc6cff8a3
amzoeee <zoe.z.zhao@gmail.com>
2025-02-12 19:31:11 -0800

    uncomment vision sanity check

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -243,12 +243,12 @@ public class RobotContainer {
     if (estimatedRobotPose != null) {
       Pose2d estPose2d = estimatedRobotPose.estimatedPose.toPose2d();
     
-      // check if new estimated pose and previous pose are less than 1 meter apart
-      // if (estPose2d.getTranslation().getDistance(drivetrain.getState().Pose.getTranslation()) < 1) {
+      // check if new estimated pose and previous pose are less than 2 meters apart
+      if (estPose2d.getTranslation().getDistance(drivetrain.getState().Pose.getTranslation()) < 2) {
         drivetrain.poseEstimator.addVisionMeasurement(estPose2d, estimatedRobotPose.timestampSeconds);
+      }
 
-        m_estPoseField.setRobotPose(estPose2d);
-      // }
+      m_estPoseField.setRobotPose(estPose2d);
     }
 
     m_actualField.setRobotPose(drivetrain.getState().Pose);
