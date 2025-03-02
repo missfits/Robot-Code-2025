5dbe8bc5bba1e54d6ade2c0766753f6325bf0851
amzoeee <zoe.z.zhao@gmail.com>
2025-03-02 00:04:39 -0800

    add comments for bindings

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -177,38 +177,47 @@ public class RobotContainer {
     ); 
 
     // set move to state
+    // C4 -> rightTrigger; !pov (any), !start
     copilotJoystick.rightTrigger().and(copilotJoystick.povCenter()).and(copilotJoystick.start().negate()).onTrue(
       m_lifter.moveToCommand(RobotState.L4_CORAL)
     ); 
 
+    // C4_WITH_CORAL -> rightTrigger + start; !pov (any)
     copilotJoystick.rightTrigger().and(copilotJoystick.povCenter()).and(copilotJoystick.start()).onTrue(
       m_lifter.moveToCommand(RobotState.L4_CORAL_WITH_CORAL)
     ); 
 
+    // C3 -> leftTrigger; !pov (any), !start
     copilotJoystick.leftTrigger().and(copilotJoystick.povCenter()).and(copilotJoystick.start().negate()).onTrue(
       m_lifter.moveToCommand(RobotState.L3_CORAL)
     ); 
 
+    // C3_WITH_CORAL -> leftTrigger + start; !pov (any)
     copilotJoystick.leftTrigger().and(copilotJoystick.povCenter()).and(copilotJoystick.start()).onTrue(
       m_lifter.moveToCommand(RobotState.L3_CORAL_WITH_CORAL)
     ); 
 
+    // C2 -> rightBumper; !a, !pov (any), !start
     copilotJoystick.rightBumper().and(copilotJoystick.a().negate()).and(copilotJoystick.povCenter()).and(copilotJoystick.start().negate()).onTrue(
       m_lifter.moveToCommand(RobotState.L2_CORAL)
     ); 
 
+    // C2_WITH_CORAL -> rightBumper + start; !a, !pov (any)
     copilotJoystick.rightBumper().and(copilotJoystick.a().negate()).and(copilotJoystick.povCenter()).and(copilotJoystick.start()).onTrue(
       m_lifter.moveToCommand(RobotState.L2_CORAL_WITH_CORAL)
     ); 
 
+    // C1 -> leftBumper; !a, !pov (any)
     copilotJoystick.leftBumper().and(copilotJoystick.a().negate()).and(copilotJoystick.povCenter()).onTrue(
       m_lifter.moveToCommand(RobotState.L1_CORAL)
     ); 
 
+    // A2 -> rightBumper + a; !pov (any)
     copilotJoystick.rightBumper().and(copilotJoystick.a()).and(copilotJoystick.povCenter()).onTrue(
       m_lifter.moveToCommand(RobotState.L2_ALGAE)
     ); 
 
+    // A3 -> leftBumper + a; !pov (any)
     copilotJoystick.leftBumper().and(copilotJoystick.a()).and(copilotJoystick.povCenter()).onTrue(
       m_lifter.moveToCommand(RobotState.L3_ALGAE)
     ); 
