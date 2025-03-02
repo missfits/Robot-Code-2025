ff039ce3e389b7826ed773bbec93038d445f9cee
amzoeee <zoe.z.zhao@gmail.com>
2025-03-01 23:57:10 -0800

    add with coral constants and robot state

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -191,10 +191,16 @@ public final class Constants {
     public static final double C1_ARM_POS = ArmConstants.INITIAL_POSITION;
     public static final double C2_ELEVATOR_POS = 0.3611; // from 3/1
     public static final double C2_ARM_POS = 0.6870; // from 3/1
+    public static final double C2_ELEVATOR_POS_WITH_CORAL = 0.3172; // from 3/1
+    public static final double C2_ARM_POS_WITH_CORAL = 1.0371; // from 3/1
     public static final double C3_ELEVATOR_POS = 0.7124; // from 3/1
     public static final double C3_ARM_POS = 0.7930; // from 3/1
+    public static final double C3_ELEVATOR_POS_WITH_CORAL = 0.7124; // from 3/1
+    public static final double C3_ARM_POS_WITH_CORAL = 1.0515; // from 3/1
     public static final double C4_ELEVATOR_POS = 0.7043; // from 3/1
     public static final double C4_ARM_POS = -2.9187; // from 3/1
+    public static final double C4_ELEVATOR_POS_WITH_CORAL = 0.7026; // from 3/1
+    public static final double C4_ARM_POS_WITH_CORAL = -3.2239; // from 3/1
     public static final double A2_ELEVATOR_POS = 0; // from 3/1
     public static final double A2_ARM_POS = 1.1532; // from 3/1
     public static final double A3_ELEVATOR_POS = 0.3611; // from 3/1
