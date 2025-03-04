b38a908fcb29ed026572283203fac6dd19b25e41
amzoeee <zoe.z.zhao@gmail.com>
2025-03-03 18:28:01 -0800

    increase elevator and arm voltage limits

--- src/main/java/frc/robot/subsystems/lifter/ArmIOHardware.java
+++ src/main/java/frc/robot/subsystems/lifter/ArmIOHardware.java
@@ -62,7 +62,7 @@ public class ArmIOHardware {
     }
 
     public void setVoltage(double value) {
-        value = MathUtil.clamp(value, -5, 5);
+        value = MathUtil.clamp(value, -8, 8);
         m_armMotor.setControl(new VoltageOut(value));
         SmartDashboard.putNumber("arm/voltage", value);
     }
