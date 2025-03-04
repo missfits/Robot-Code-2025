74b988efca3aec953e35e7d887a7eabe9cc20dde
amzoeee <zoe.z.zhao@gmail.com>
2025-03-03 17:49:33 -0800

    update dynamene wheel radius fudge factor
    previously part of "temp commit"

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -49,7 +49,7 @@ public final class Constants {
     public static final double ROBOT_POSITION_D = 0;
 
     public static final double CERIDWEN_WHEEL_RADIUS_FUDGE_FACTOR = 0.96153846153; // approximated @ PF session 1/13
-    public static final double DYNAMENE_WHEEL_RADIUS_FUDGE_FACTOR = 0.96153846153; // approximated @ PF session 1/13
+    public static final double DYNAMENE_WHEEL_RADIUS_FUDGE_FACTOR = 0.949; // approximated @ PF session 1/13
 
   }
 
