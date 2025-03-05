c4f423269804f62c427fdedc1e372672db7bf9dd
riley <rileykjen@gmail.com>
2025-03-04 19:04:09 -0800

    add climb sequence commands

--- src/main/java/frc/robot/subsystems/climber/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberSubsystem.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems.climber;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ArmConstants;
@@ -9,6 +10,7 @@ import frc.robot.subsystems.lifter.ArmIOHardware;
 
 public class ClimberSubsystem extends SubsystemBase {
     private final ClimberIOHardware m_IO = new ClimberIOHardware();
+    private boolean m_deployed = false;
 
     public Command runClimberOff() {
         return new RunCommand(
@@ -26,6 +28,30 @@ public class ClimberSubsystem extends SubsystemBase {
     public Command manualMoveBackwardCommand() {
         return new RunCommand(
             () -> m_IO.setVoltage(-ClimberConstants.MANUAL_MOVE_MOTOR_SPEED),
+            this
+        );
+    }
+
+    public Command climbSequenceCommand() {
+        return m_deployed ? liftRobotCommand() : deployClimberCommand();
+    }
+
+    public Command deployClimberCommand() {
+        return new FunctionalCommand(
+            () -> m_deployed = true,
+            () -> m_IO.setVoltage(ClimberConstants.AUTO_MOVE_MOTOR_SPEED),
+            (interrupted) -> m_IO.motorOff(),
+            () -> (m_IO.getPosition() > ClimberConstants.DEPLOY_POSITION),
+            this
+        );
+    }
+
+    public Command liftRobotCommand() {
+        return new FunctionalCommand(
+            () -> {},
+            () -> m_IO.setVoltage(ClimberConstants.AUTO_MOVE_MOTOR_SPEED),
+            (interrupted) -> m_IO.motorOff(),
+            () -> (m_IO.getPosition() > ClimberConstants.FINAL_POSITION),
             this
         );
     }
