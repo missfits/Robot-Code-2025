8dc889abb80cfd2932ec54eff7cd321da9dc657e
amzoeee <zoe.z.zhao@gmail.com>
2025-03-15 09:39:35 -0700

    Update at 'Sat Mar 15 09:39:34 PDT 2025'

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -43,6 +43,7 @@ import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
@@ -430,6 +431,13 @@ public class RobotContainer {
     m_lifter.setDisabledNeutralMode();
     m_climber.setDisabledNeutralMode();
 
+  }
+
+  public void setVoltageUponDisable() {
+    CommandScheduler.getInstance().schedule(m_elevator.setVoltageToZeroCommand());
+    CommandScheduler.getInstance().schedule(m_arm.setVoltageToZeroCommand());
+    CommandScheduler.getInstance().schedule(m_collar.setVoltageToZeroCommand());
+    CommandScheduler.getInstance().schedule(drivetrain.getCommandFromRequest(() -> drive.withVelocityX(0).withVelocityY(0).withRotationalRate(0)));
   }
 
   public Command getAutonomousCommand() {
