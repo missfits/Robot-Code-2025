c2b019d360db2bf33a8600cebef236540813490a
amzoeee <zoe.z.zhao@gmail.com>
2025-03-29 11:45:17 -0700

    reset collar encoder upon seeing coral and log

--- src/main/java/frc/robot/subsystems/collar/CollarCommandFactory.java
+++ src/main/java/frc/robot/subsystems/collar/CollarCommandFactory.java
@@ -29,6 +29,7 @@ public class CollarCommandFactory {
         return Commands.sequence(
             m_collar.runCollar(CollarConstants.INTAKE_SECONDARY_BACK_MOTOR_SPEED).until(m_rampSensors.coralSeenAfterRampTrigger().or(m_rampSensors.coralSeenInRampTrigger())),
             m_collar.runCollar(CollarConstants.INTAKE_MOTOR_SPEED).until(m_rampSensors.coralSeenAfterRampTrigger()),
+            m_collar.resetEncoders(),
             m_collar.runCollar(CollarConstants.INTAKE_SECONDARY_MOTOR_SPEED).until(m_rampSensors.coralSeenAfterRampTrigger().negate()),
             m_collar.runCollar(CollarConstants.INTAKE_SECONDARY_BACK_MOTOR_SPEED).withTimeout(CollarConstants.INTAKE_BACKWARDS_TIMEOUT),
             m_collar.runCollarOffInstant()).withName("intakeCoralSequence2");
