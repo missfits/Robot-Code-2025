dc6b66d5ed7fcfab9dca4249fe06bd0a85b7ca0c
zoe <zoe.z.zhao@gmail.com>
2025-04-01 20:07:55 -0700

    make autoalign faster; change intermed. pos dist and green light tolerance

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -193,9 +193,9 @@ public final class Constants {
     public static final double REEF_OFFSET_RIGHT = Units.inchesToMeters(2);
     public static final double REEF_OFFSET_LEFT = Units.inchesToMeters(12);
 
-    public static final double kMaxV = 1; // to be tuned
-    public static final double kMaxA = 1; // to be tuned
-    public static final double INTERMEDIATE_POS_DIST = Units.inchesToMeters(2);
+    public static final double kMaxV = 2; // to be tuned
+    public static final double kMaxA = 1.5; // to be tuned
+    public static final double INTERMEDIATE_POS_DIST = Units.inchesToMeters(5);
   }
 
   public static class VisionConstants {
@@ -223,7 +223,7 @@ public final class Constants {
     public static final double MAX_VISION_POSE_PITCH = 0.05; // in radians
     public static final double VISION_DISTANCE_DISCARD = 10; 
 
-    public static final double VISION_ALIGNMENT_DISCARD = Units.inchesToMeters(2); // in meters
+    public static final double VISION_ALIGNMENT_DISCARD = Units.inchesToMeters(1.5); // in meters
 
 
     public static final Translation2d ROBOT_TO_CAM1 = 
