e2cb4e35e24733cab84790c35150a2f02d4674f9
zoe <zoe.z.zhao@gmail.com>
2025-04-02 19:13:50 -0700

    log battery voltage

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,8 +4,10 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.Constants.RobotConstants;
@@ -26,6 +28,8 @@ public class Robot extends TimedRobot {
   public void robotPeriodic() {
     CommandScheduler.getInstance().run(); 
     m_robotContainer.updatePoseEst();
+
+    SmartDashboard.putNumber("batteryVoltage", RobotController.getBatteryVoltage());
   }
 
   @Override
