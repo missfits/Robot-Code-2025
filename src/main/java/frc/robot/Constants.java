469c4d4b0570cd4a5295ef3aeee8b23e0bd434e2
zoe <zoe.z.zhao@gmail.com>
2025-05-08 18:47:13 -0700

    tweak climber speed and degrees per rot

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -185,9 +185,9 @@ public final class Constants {
 
     public static final double MOTOR_STATOR_LIMIT = 40; // needs to be updated lol
 
-    public static final double MANUAL_MOVE_MOTOR_SPEED = 3.0;
-    public static final double AUTO_MOVE_MOTOR_SPEED = 12.0; // filler
-    public static final double DEGREES_PER_ROTATION = 360.0/810.0;
+    public static final double MANUAL_MOVE_MOTOR_SPEED = 6.0;
+    public static final double AUTO_MOVE_MOTOR_SPEED = 6.0; 
+    public static final double DEGREES_PER_ROTATION = 360.0/690.0;
 
     public static final double INITIAL_POSITION = 0.0; // down
     public static final double DEPLOY_POSITION = -2.58; // 120 degrees
